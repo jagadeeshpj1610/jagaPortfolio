bd1455438973879d69f26f473eb8e443770f81fd
JAGADEESH <22022cm040@gmail.com>
2025-05-21 20:27:37 +0530

    PJ | add labels to skills and made some changes on css

--- portFolio/src/components/skills/skills.jsx
+++ portFolio/src/components/skills/skills.jsx
@@ -23,42 +23,55 @@ const Skills = () => {
       <div className="skillsImagesDivs">
         <div className="skill">
           <img src={htmlIcon} alt="html" />
+          <h3>HTML</h3>
         </div>
         <div className="skill">
           <img src={cssIcon} alt="css" />
+          <h3>CSS</h3>
         </div>
         <div className="skill">
           <img src={jsIcon} alt="js" />
+          <h3>Javascript</h3>
         </div>
         <div className="skill">
           <img src={reactIcon} alt="react" />
+          <h3>React Js</h3>
         </div>
         <div  className="skill">
           <img src={nodeIcon} alt="node" />
+          <h3>Node JS</h3>
         </div>
         <div  className="skill">
           <img src={mysqlIcon} alt="mysql" />
+          <h3>MYSQL</h3>
         </div>
         <div className="skill">
           <img src={mongodbIcon} alt="mongodb" />
+          <h3>Mongodb</h3>
         </div>
         <div className="skill">
           <img src={pythonIcon} alt="python" />
+          <h3>Python</h3>
         </div>
         <div className="skill">
           <img src={git} alt="" />
+          <h3>Git</h3>
         </div>
         <div className="skill">
           <img src={githubIcon} alt="github" />
+          <h3>Github</h3>
         </div>
         <div className="skill">
           <img src={vscodeIcon} alt="vscodeIcon" />
+          <h3>VS Code</h3>
         </div>
         <div className="skill">
           <img src={slack} alt="slack" />
+          <h3>Slack</h3>
         </div>
         <div className="skill">
           <img src={npm} alt="npm" />
+          <h3>npm</h3>
         </div>
       </div>
     </div>
