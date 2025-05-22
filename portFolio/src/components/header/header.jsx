c54348f8695bc1a16c4137a4024545b57d2f1fd7
JAGADEESH <22022cm040@gmail.com>
2025-05-22 15:32:49 +0530

    made some changes for small screens

--- portFolio/src/components/header/header.jsx
+++ portFolio/src/components/header/header.jsx
@@ -1,26 +1,3 @@
-// import "../header/header.css"
-
-// const Header = () => {
-//   return (
-//     <div className="navDiv" id="nav">
-//       <div className="childOne">
-//         <h3><a href="#nav">Jagadeesh Pachikura</a></h3>
-//       </div>
-//       <div className="childSecond">
-//         <ul className="navList">
-//           <li className="navItem"><a href="#about">ABOUT</a></li>
-//           <li className="navItem"><a href="#skills">SKILLS</a></li>
-//           <li className="navItem"><a href="#experience">EXPERIENCE</a></li>
-//           <li className="navItem"><a href="#projects">PROJECTS</a></li>
-//           <li className="navItem"><a href="#contact">CONTACT ME</a></li>
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Header
-
 import React, { useState } from "react";
 import "../header/header.css";
 
@@ -35,14 +12,12 @@ const Header = () => {
         <h3><a href="#nav">Jagadeesh Pachikura</a></h3>
       </div>
 
-      {/* Hamburger icon for small screens */}
       <div className="hamburger" onClick={toggleMenu}>
         <div className={`line ${isOpen ? "open" : ""}`}></div>
         <div className={`line ${isOpen ? "open" : ""}`}></div>
         <div className={`line ${isOpen ? "open" : ""}`}></div>
       </div>
 
-      {/* Navigation links */}
       <div className={`childSecond ${isOpen ? "open" : ""}`}>
         <ul className="navList">
           <li className="navItem"><a href="#about" onClick={() => setIsOpen(false)}>ABOUT</a></li>
