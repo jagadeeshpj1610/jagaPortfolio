5c90cc8caa1256b11b2a62ca7a14c1235c9a7dfb
JAGADEESH <22022cm040@gmail.com>
2025-05-22 11:08:17 +0530

    PJ | Done footer component

---
+++ portFolio/src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "../footer/footer.css"
+
+
+const Footer = () =>{
+  return(
+    <div  className="footer">
+      <p>Designed and Build by Jaga</p>
+      <p>© 2025 Jagadeesh Pachikura</p>
+    </div>
+  )
+}
+
+export default Footer
--- portFolio/src/components/main/main.jsx
+++ portFolio/src/components/main/main.jsx
@@ -4,6 +4,7 @@ import Skills from "../skills/skills"
 import Experience from "../experience/experience"
 import Projects from "../projects/projects"
 import ContactMe from "../contactMe/contactMe"
+import Footer from "../footer/footer"
 
 
 const Main = () => {
@@ -26,6 +27,9 @@ const Main = () => {
       </div>
       <div className="contactMe">
         <ContactMe />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   )
