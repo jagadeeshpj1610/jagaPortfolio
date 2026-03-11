8547f4f63a1ef7582a39381af2ecd9c36fcd5d9a
Jagadeesh <22022cm040@gmail.com>
2026-03-11 11:34:51 +0530

    change he projects deploy url

--- portFolio/src/components/projects/projects.jsx
+++ portFolio/src/components/projects/projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
               <span className="stackTag">Responsive</span>
             </div>
             <div className="hlo">
-              <a href="YOUR_LIVE_URL_HERE" target="_blank" rel="noreferrer">
+              <a href="https://jaga-portfolio1610.netlify.app/" target="_blank" rel="noreferrer">
                 <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
                 View Website
               </a>
