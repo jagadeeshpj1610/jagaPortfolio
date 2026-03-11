ff7bdc827c7c4a21ea4a11f7d839df1509d2bd85
Jagadeesh <22022cm040@gmail.com>
2026-03-11 10:25:14 +0530

    change some text

--- portFolio/src/components/header/header.jsx
+++ portFolio/src/components/header/header.jsx
@@ -3,13 +3,12 @@ import "../header/header.css";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <div className="navDiv" id="nav">
       <div className="childOne">
-        <h3><a href="#nav">Jagadeesh Pachikura</a></h3>
+        <h3><a href="#nav">JP.</a></h3>
       </div>
 
       <div className="hamburger" onClick={toggleMenu}>
@@ -20,11 +19,11 @@ const Header = () => {
 
       <div className={`childSecond ${isOpen ? "open" : ""}`}>
         <ul className="navList">
-          <li className="navItem"><a href="#about" onClick={() => setIsOpen(false)}>ABOUT</a></li>
-          <li className="navItem"><a href="#skills" onClick={() => setIsOpen(false)}>SKILLS</a></li>
-          <li className="navItem"><a href="#experience" onClick={() => setIsOpen(false)}>EXPERIENCE</a></li>
-          <li className="navItem"><a href="#projects" onClick={() => setIsOpen(false)}>PROJECTS</a></li>
-          <li className="navItem"><a href="#contact" onClick={() => setIsOpen(false)}>CONTACT ME</a></li>
+          <li className="navItem"><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
+          <li className="navItem"><a href="#skills" onClick={() => setIsOpen(false)}>Skills</a></li>
+          <li className="navItem"><a href="#experience" onClick={() => setIsOpen(false)}>Experience</a></li>
+          <li className="navItem"><a href="#projects" onClick={() => setIsOpen(false)}>Projects</a></li>
+          <li className="navItem"><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
         </ul>
       </div>
     </div>
